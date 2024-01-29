9de3ba84fcf2a10fac1587115460e1bac5131f77
AryamanTandon18 <tandon.aryaman1@gmail.com>
2024-01-29 18:25:51 +0530

    last

--- utils/features.js
+++ utils/features.js
@@ -6,8 +6,8 @@ export const sendCookie=(res,user,message)=>{
     res.status(200).cookie("Token" ,token ,{
         httpOnly:true, //so that cookies can be modified in the server only not from the use end
         maxAge:7*24*60*60*1000,    //7 days this is always in miliSecond
-        // sameSite: process.env.node_env ==="Development"?"lax": "none",               
-        // secure:process.env.node_env ==="Development"? false: true,           
+        sameSite: process.env.node_env ==="Development"?"lax": "none",               
+        secure:process.env.node_env ==="Development"? false: true,           
     }).json({
         sucess:true,
         message,    
